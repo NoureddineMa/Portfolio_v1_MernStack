fa87da710e6a56e213a8c58ea60adb9d7b6748ee
NoureddineMa <noureddinemaher13@gmail.com>
2022-12-31 17:14:10 +0100

    End all functions blog

--- server/Controllers/BlogController.js
+++ server/Controllers/BlogController.js
@@ -19,7 +19,7 @@ const PostBlog = asyncHandler(async (req, res) => {
         Content
     })
     if (blog) {
-        res.status(201)
+        res.status(200)
         .json({message: "Blog created successfully"})
     }else  {
         res.status(400)
@@ -27,4 +27,76 @@ const PostBlog = asyncHandler(async (req, res) => {
     }
 })
 
-module.exports = PostBlog;
+// @desc Get all Blog
+// @access Private
+// URL : http://localhost:3001/api/blog/getAll
+
+const GetAllBlog = asyncHandler(async (req, res) => {
+    const blogs = await Blog.find({});
+    if(blogs){
+        res.status(200)
+        .json(blogs)
+    } else {
+        res.status(404)
+        .json({message: "Blogs not found"})
+    }
+})
+
+// @desc update Blog By id 
+// @access Private
+// URL : http://localhost:3001/api/blog/update/:id
+
+const UpdateBlog = asyncHandler(async (req, res) => {
+    const { title, Image, Content } = req.body;
+    if ( !title || !Image || !Content ){
+        res.status(400)
+        .json({message: "Please fill all the fields"})
+    }
+    const blog = await Blog.findById(req.params.id);
+    if(blog){
+        blog.title = title;
+        blog.Image = Image;
+        blog.Content = Content;
+        const updatedBlog = await blog.save();
+        res.status(200)
+        .json(updatedBlog)
+    } else {
+        res.status(404)
+        .json({message: "Blog not found"})
+    }
+})
+
+
+// @desc delete Blog By id 
+// @access Private
+// URL : http://localhost:3001/api/blog/delete/:id
+
+const DeleteBlog = asyncHandler(async (req, res) => {
+    const blog = await Blog.findById(req.params.id);
+    if(blog){
+        await blog.delete();
+        res.status(200)
+        .json({message: "Blog removed succefully"})
+    } else {
+        res.status(404)
+        .json({message: "Blog not found"})
+    }
+})
+
+// @desc get Blog By id 
+// @access Private
+// URL : http://localhost:3001/api/blog/getById/:id
+
+const GetBlogById = asyncHandler(async (req, res) => {
+    const blog = await Blog.findById(req.params.id);
+    if(blog){
+        res.status(200)
+        .json(blog)
+    } else {
+        res.status(404)
+        .json({message: "Blog not found"})
+    }
+})
+
+
+module.exports = {PostBlog , GetAllBlog , UpdateBlog , DeleteBlog , GetBlogById}
--- server/Routers/BlogRouters.js
+++ server/Routers/BlogRouters.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const PostBlog = require('../Controllers/BlogController');
+const {PostBlog, GetAllBlog , UpdateBlog , DeleteBlog , GetBlogById} = require('../Controllers/BlogController');
 
 router.post('/create', PostBlog);
+router.get('/getAll', GetAllBlog);
+router.put('/update/:id', UpdateBlog);
+router.delete('/delete/:id', DeleteBlog);
+router.get('/getById/:id', GetBlogById);
+
 
 module.exports = router
